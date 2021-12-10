a8da1e900a7dc370938cc970685083029ea03e66
santi <santisevitz@hotmail.com>
2021-12-10 17:12:58 -0300

    first commit

---
+++ Models/ToDo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool Finished { get; set; }
+        public virtual ApplicationUser User { get; set; }
+
+
+
+
+    }
+}
